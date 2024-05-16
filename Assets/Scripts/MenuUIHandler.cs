12f528c73f8d28d4c519e6e3e2aedc5f6f1d71c8
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-05-16 18:26:12 +0100

    JSON colour save
    Saving and loading colour chosen in the main menu

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -15,10 +15,11 @@ using UnityEditor;
 public class MenuUIHandler : MonoBehaviour
 {
     public ColorPicker ColorPicker;
+    public MainManager MainManager;
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        MainManager.NewColorSelected(color);
     }
 
     private void Start()
@@ -26,6 +27,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 
     public void StartNew()
@@ -35,10 +37,21 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
         Application.Quit(); 
 #endif
+    }
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
